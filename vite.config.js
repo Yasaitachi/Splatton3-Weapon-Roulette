587e46b1c8c62609f9537ff693a8bb21c44b165e
Yasaitachi <takeru.20120130@gmail.com>
2025-11-13 17:46:17 +0900

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
   plugins: [react()],
-  base: "./",      // ← これが必須
+  base: "./",        // Firebase Hosting では相対パスが必須
   build: {
-    outDir: "dist"
+    outDir: "dist"   // Firebase hosting と同じディレクトリ
   }
 })
