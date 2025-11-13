a2c1123e67897f048d899427df2afe70c20354e1
Yasaitachi <takeru.20120130@gmail.com>
2025-11-13 17:29:40 +0900

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",  // Firebaseでは相対パス必須
+  base: "./",   // これが重要
   build: {
-    outDir: "dist"  // デフォルトでOK
+    outDir: "dist"
   }
 })
