eec0b15b412048cc10d3cd37cb248a634145f757
Yasaitachi <takeru.20120130@gmail.com>
2025-11-13 19:50:57 +0900

    ver.1.0.0

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/",      // Firebase Hosting の rewrites を使う場合は絶対パスにする
+  base: "./",
   build: {
-    outDir: "dist" // Firebase hosting の public と同じ
+    outDir: "dist"
   }
-})
+});
